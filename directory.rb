1eb883ab2f51b8e56209d93dde392a9499adef28
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-09 13:39:46 +0000

    Amount of students

--- directory.rb
+++ directory.rb
@@ -1,43 +1,14 @@
-student_count = 11
-students = [
-{name: "Dr. Hannibal Lecter", cohort: :november},
-{name: "Darth Vader", cohort: :november},
-{name: "Nurse Ratched", cohort: :november},
-{name: "Michael Corleone", cohort: :november},
-{name: "Alex DeLarge", cohort: :november},
-{name: "The Wicked Witch of the West", cohort: :november},
-{name: "Terminator", cohort: :november},
-{name: "Freddy Krueger", cohort: :november},
-{name: "The Joker", cohort: :november},
-{name:"Joffrey Baratheon", cohort: :november},
-{name:"Norman Bates", cohort: :november}
-]
-def enter_students
-  puts "Please enter the names of the students"
-  puts "To finish, just hit return twice"
-  students = []
-  name = gets.chomp
-  while !name.empty? do
-    students << {name: name, chohort: :november}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp 
-  end
-  students
-end
-
-def header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end
-def lists(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} chohort)"
-  end
-end
-def footer(names)
-  puts "Overall, we have #{names.count} great students"
-end
-students = enter_students
-header
-print (students)
-footer(students)
+puts "The students of Villains Academy"
+puts "-------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+print "Overall we have 11 great sudents"
