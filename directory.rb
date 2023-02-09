253e15686a925e0a504ab060e574a07f66cd608b
KumarS14 <sonjaykumar1404@gmail.com>
2023-02-09 13:50:47 +0000

    Turned puts into hash

--- directory.rb
+++ directory.rb
@@ -1,14 +1,20 @@
-puts "The students of Villains Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-print "Overall we have 11 great sudents"
+students = [
+  {name: "The students of Villains Academy"},
+  {name: "-------------"},
+  {name: "Dr. Hannibal Lecter"},
+  {name: "Darth Vader"},
+  {name: "Nurse Ratched"},
+  {name: "Michael Corleone"},
+  {name: "Alex DeLarge"},
+  {name: "The Wicked Witch of the West"},
+  {name: "Terminator"},
+  {name: "Freddy Krueger"},
+  {name: "The Joker"},
+  {name: "Joffrey Baratheon"},
+  {name: "Norman Bates"},
+  {name: "Overall we have 11 great sudents"}
+]
+
+students.each do |student|
+  puts student[:name]
+end
